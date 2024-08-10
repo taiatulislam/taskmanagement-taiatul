a7ef2903d7ba08888389c8b9e673923ceae45242
taiatulislam <taiatulislamapon@gmail.com>
2024-08-10 13:53:13 +0600

    initial file setup with react, typescript and added topbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-10 mt-10">
+      <div className='bg-[#ECEDEE] rounded-xl flex items-center justify-between p-5'>
+        <div className=''>
+          <input type="text" placeholder='Search Project' className='bg-white rounded-xl p-2 w-[400px] outline-none' />
+        </div>
+        <div>
+          <label htmlFor="filter">Filter</label>
+          <select name="filter" id="" defaultValue={""}>
+            <option value="name">Name</option>
+            <option value="value">Value</option>
+            <option value="type">Type</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
